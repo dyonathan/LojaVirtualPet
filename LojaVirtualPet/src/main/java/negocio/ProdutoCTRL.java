d2d1ee267ca4e981b1c914b4c2f96dc5d10390e2
SUPORTE YES <SUPORTE YES@ALUNOYES-PC>
2017-12-13 19:51:52 -0200

    Terminei o primeiro pdf

---
+++ LojaVirtualPet/src/main/java/negocio/ProdutoCTRL.java
@@ -0,0 +1,55 @@
+package negocio;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import beans.Produto;
+import persistencia.ProdutoDAO;
+
+@ManagedBean
+@SessionScoped
+public class ProdutoCTRL  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Produto produto;
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	public List<Produto> getListagem() {
+		return ProdutoDAO.listagem("");
+	}
+	
+	public String actionGravar() {
+		if (produto.getId() == 0) {
+			ProdutoDAO.inserir(produto);
+			return actionInserir();
+		}else{
+			ProdutoDAO.alterar(produto);
+			return "lista_produto";
+		}
+	}
+	
+	public String actionInserir() {
+		produto = new Produto();
+		return "form_produto";
+	}
+	
+	public String actionExcluir(Produto p) {
+		ProdutoDAO.excluir(p);
+		return "lista_produto";
+	}
+	
+	public String actionAlterar(Produto p) {
+		produto = p;
+		return "form_produto";
+	}
+
+}
--- LojaVirtualPet/src/main/java/persistencia/ProdutoDAO.java
+++ LojaVirtualPet/src/main/java/persistencia/ProdutoDAO.java
@@ -1,6 +1,7 @@
 package persistencia;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.hibernate.*;
 
@@ -12,7 +13,41 @@ public class ProdutoDAO implements Serializable {
 	
 	public static void inserir(Produto produto) {
 		Session sessao = HibernateUtil.getSessionFactory().openSession();
-		Transaction
+		Transaction t = sessao.beginTransaction();
+		sessao.save(produto);
+		t.commit();
+		sessao.close();
+	}
+	
+	public static void alterar(Produto produto) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = sessao.beginTransaction();
+		sessao.update(produto);
+		t.commit();
+		sessao.close();
+	}
+	
+	public static void excluir(Produto produto) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = sessao.beginTransaction();
+		sessao.delete(produto);
+		t.commit();
+		sessao.close();
+	}
+	
+	public static List<Produto> listagem(String filtro){
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Query consulta;
+		if (filtro.trim().length() == 0) {
+			consulta = sessao.createQuery("from Produto order by pro_nome");
+		}else {
+			consulta = sessao.createQuery("from Produto "
+					+ "where pro_nome like :parametro order by pro_nome");
+			consulta.setString("parametro", filtro + "%");
+		}
+		List lista = consulta.list();
+		sessao.close();
+		return lista;
 	}
 
 }
